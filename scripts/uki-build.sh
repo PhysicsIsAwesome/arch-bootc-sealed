c9b0e57ab0c70978b43db7c551c9c207e6e94132
PhysicsIsAwesome <69597878+PhysicsIsAwesome@users.noreply.github.com>
2026-05-21 17:27:49 +0200

    Add --measure

--- scripts/uki-build.sh
+++ scripts/uki-build.sh
@@ -4,5 +4,6 @@ cmdline='"lsm=landlock,lockdown,yama,integrity,apparmor,bpf audit=1 audit_backlo
 ukify_args="\
             --secureboot-private-key /run/secrets/secureboot_key \
             --secureboot-certificate /run/secrets/secureboot_cert \
+            --measure \
             --output /boot/${kver}.efi"
 bootc container ukify --rootfs /target --karg "rw rootflags=subvol=root rd.luks.name=7fa68618-91b8-4cf6-8a30-6c662331b0c7=luks-7fa68618-91b8-4cf6-8a30-6c662331b0c7 root=/dev/mapper/luks-7fa68618-91b8-4cf6-8a30-6c662331b0c7" -- ${ukify_args}
