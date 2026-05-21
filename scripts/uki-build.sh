149b0260ff6a849b220f19113254131f73406f55
PhysicsIsAwesome <69597878+PhysicsIsAwesome@users.noreply.github.com>
2026-05-21 17:27:22 +0200

    Change boot parameter for tests

--- scripts/uki-build.sh
+++ scripts/uki-build.sh
@@ -5,4 +5,4 @@ ukify_args="\
             --secureboot-private-key /run/secrets/secureboot_key \
             --secureboot-certificate /run/secrets/secureboot_cert \
             --output /boot/${kver}.efi"
-bootc container ukify --rootfs /target -- ${ukify_args}
+bootc container ukify --rootfs /target --karg "rw rootflags=subvol=root rd.luks.name=7fa68618-91b8-4cf6-8a30-6c662331b0c7=luks-7fa68618-91b8-4cf6-8a30-6c662331b0c7 root=/dev/mapper/luks-7fa68618-91b8-4cf6-8a30-6c662331b0c7" -- ${ukify_args}
